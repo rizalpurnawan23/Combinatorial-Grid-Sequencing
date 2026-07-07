7b62a59028ce28f3deeb1b8be95be99c4dd57586
Rizal Purnawan <104172029+rizalpurnawan23@users.noreply.github.com>
2026-07-08 01:41:26 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Combinatorial-Grid-Sequencing',
+    name='cgs-prototype',
     version='0.0.5',
     packages=find_packages(),
     install_requires=[
