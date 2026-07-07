229cebc39d43a196dbd631df63a28007e6636fb5
Rizal Purnawan <104172029+rizalpurnawan23@users.noreply.github.com>
2026-07-08 01:35:27 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cgsMPDT1',
-    version='0.0.1',
+    name='Combinatorial-Grid-Sequencing',
+    version='0.0.5',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.23.5',
         'pandas>=1.5.3'
         ],
-    url='https://github.com/rizalpurnawan23/CGS-MPDT1'
+    url='https://github.com/rizalpurnawan23/Combinatorial-Grid-Sequencing'
 )
